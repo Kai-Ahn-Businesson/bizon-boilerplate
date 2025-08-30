f9889c64551c09788c7817e1836bd0fffd616fae
Kai Ahn <kai.ahn@businesson.co.kr>
2025-08-30 20:54:36 +0900

    스프링 HATEOAS 스타터 의존성
    spring-boot-starter-batch
    
    Signed-off-by: Kai Ahn <kai.ahn@businesson.co.kr>
    
    #1

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-batch")
+    implementation("org.springframework.boot:spring-boot-starter-hateoas")
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
