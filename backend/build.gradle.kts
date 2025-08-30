8130ad7531a94788a190144d3b4ef1d73c0996f2
Kai Ahn <kai.ahn@businesson.co.kr>
2025-08-30 20:53:01 +0900

    스프링 배치 스타터 의존성
    spring-boot-starter-batch
    
    Signed-off-by: Kai Ahn <kai.ahn@businesson.co.kr>
    
    #1

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -44,6 +44,7 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-batch")
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
