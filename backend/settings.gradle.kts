210b384647f03c70955d4dda238cc0afa993745a
Kai Ahn <kai.ahn@businesson.co.kr>
2025-08-31 10:12:58 +0900

    pluginManagement - 버전 변수 정의
    Signed-off-by: Kai Ahn <kai.ahn@businesson.co.kr>
    
    #1

--- backend/settings.gradle.kts
+++ backend/settings.gradle.kts
@@ -1,1 +1,9 @@
 rootProject.name = "backend"
+
+pluginManagement {
+    // 버전 변수 정의
+    val springBootVersion = "3.5.5"
+    val kotlinVersion = "2.2.10"
+    val dependencyManagementVersion = "1.1.7"
+    repositories {}
+}
